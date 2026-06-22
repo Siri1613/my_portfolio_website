4adc207c33e051673eaa12b7ac08fdfb9c9dda13
Kshatri Reshmasree <reshmasree1229@gmail.com>
2026-06-22 23:12:42 +0530

    changes for the package files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my_portfolio_website/',
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+base: '/my_portfolio_website/',
+plugins: [react()],
+})
